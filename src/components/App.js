64e2f879fb0400ab855ce2b7b3312c5597f3e860
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-19 15:19:44 -0500

    changed routes and navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
-import UserSignUp from './user-sign-up'
-import {Link} from 'react-router'
-import Logout from './logout'
+import Navbar from './navbar'
+import { Link } from 'react-router'
 
-class Home extends Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <nav>
-          <div className="nav-wrapper blue darken-2">
-            <Link to="/home" className="brand-logo center">Budget Butler</Link>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><Link to="/login">Log In</Link></li>
-              <li><Link to="/transactions">Transactions</Link></li>
-              <li><Link to="/expenses">Expenses</Link></li>
-              <li><a href="collapsible.html">Goals</a></li>
-              <li><a href="badges.html">Income</a></li>
-              <li><Logout /></li>
-            </ul>
-          </div>
-        </nav>
-      {this.props.children}
+        <Navbar />
+        <div>
+          { this.props.children }
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+export default App;
