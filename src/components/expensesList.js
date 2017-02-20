14985d28cb993c994b5ccfbde59cb16a4229a1ac
github username <zylberberg.jonathan@gmail.com>
2017-02-20 18:37:30 -0500

    add expense and add income

--- src/components/expensesList.js
+++ src/components/expensesList.js
@@ -1,20 +1,41 @@
 import React, {Component} from 'react'
 import { bindActionCreators } from 'redux'
 import {connect} from 'react-redux'
-import { fetchExpenses } from '../actions'
+import { fetchExpenses, fetchIncome } from '../actions'
 import ExpensesItem from './expensesItem'
 import 'materialize-css/bin/materialize.css'
 import 'materialize-css/bin/materialize.js'
 import {Collapsible, CollapsibleItem} from 'react-materialize'
+import {Pie} from 'react-chartjs-2'
 
 class ExpensesList extends Component {
 
   componentDidMount(){
+    this.props.fetchIncome()
     this.props.fetchExpenses()
   }
 
   expensesItems(){
     return this.props.expenses.map((exp, i) => { return <ExpensesItem expense={exp} key={i}/>})
+  }
+
+  addIncome() {
+    let amount = 0 
+    this.props.income.map((income) => {
+      amount += income.amount
+      return income
+    })
+    debugger
+    return amount 
+  }
+
+  addExpenses() {
+    let amount = 0 
+    this.props.expenses.map((expense) => {
+      amount += expense.budget
+      return expense
+    })
+    return amount 
   }
 
   render(){
@@ -37,6 +58,9 @@ class ExpensesList extends Component {
             </div>
           </div>
         <br />
+        {this.addIncome()} 
+        <br />
+        {this.addExpenses()}
       </div>
     )
   }
@@ -45,12 +69,13 @@ class ExpensesList extends Component {
 
 function mapStateToProps(state){
   return {
-     expenses: state.expenses
+     expenses: state.expenses,
+     income: state.income
   }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchExpenses: fetchExpenses}, dispatch)
+  return bindActionCreators({fetchExpenses: fetchExpenses, fetchIncome: fetchIncome}, dispatch)
 }
 
 
