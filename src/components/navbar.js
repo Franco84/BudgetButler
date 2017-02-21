658db170fbb697db2cf5d1b09ec6676f96e819de
franco83-2 <franco.84@gmail.com>
2017-02-21 14:14:51 -0500

    navbar margin

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
 checkLogin (){
   if (!!sessionStorage.jwt) {
     return (
-      <ul id="nav-mobile" className="logged-in right hide-on-med-and-down">
+      <ul id="nav-mobile" className="logged-in right hide-on-med-and-down" style={{marginRight: "30px"}}>
         <li><Link to="/transactions">Transactions</Link></li>
         <li><Link to="/expenses">Expenses</Link></li>
         <li><Link to="/income">Income</Link></li>
@@ -18,7 +18,7 @@ checkLogin (){
     )
   } else {
     return (
-      <ul id="nav-mobile" className="not-logged-in right hide-on-med-and-down">
+      <ul id="nav-mobile" className="not-logged-in right hide-on-med-and-down" style={{marginRight: "30px"}}>
         <li><Link to="/signup">Sign Up</Link></li>
         <li><Link to="/login">Log In</Link></li>
       </ul>
