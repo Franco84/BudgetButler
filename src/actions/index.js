2ad03b91cb174c848cfca8147695bd62801c3033
franco83-2 <franco.84@gmail.com>
2017-02-21 12:25:35 -0500

    fixed live api link

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {browserHistory} from 'react-router'
 
-axios.defaults.baseURL = 'http://budgetbutlerapi.herokuapp.com'
+axios.defaults.baseURL = 'http://budgetbutlerapi.herokuapp.com/api/v1'
 axios.defaults.headers.common['AUTHORIZATION'] = sessionStorage.getItem('jwt')
 
 export const fetchTransactions = (callback) => {
