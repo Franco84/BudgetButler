8e9d00a97a29605c24288d2e48941e5adb466676
github username <zylberberg.jonathan@gmail.com>
2017-02-21 19:50:22 -0500

    deleted file

--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -3,7 +3,6 @@ import userReducer from './user-reducer'
 import transactionReducer from './transaction-reducer'
 import expensesReducer from './expenses-reducer'
 import incomeReducer from './income-reducer'
-import transactionsDateReducer from './transaction-date-reducer'
 
 const rootReducer = combineReducers({
   currentUser: userReducer,
