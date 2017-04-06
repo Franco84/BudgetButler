683f255910d538c3f934a9b5158ce145802cdb4f
David Weinstein <dwein9@gmail.com>
2017-04-06 11:15:45 -0400

    added guest login button

--- src/components/login.js
+++ src/components/login.js
@@ -13,27 +13,34 @@ class LogIn extends Component {
       this.props.authenticateUser(user)
   }
 
+  onClick(event) {
+    event.preventDefault()
+    const user = { email: "ian@ian.ian", password: "password" }
+    this.props.authenticateUser(user)
+  }
+
   render() {
     return (
       <div className="container">
-          <div className="row">
-            <div className="col l6 m6 s6 offset-l3 offset-m3 offset-s3 center">
-              <br />
-              <div style={{color: 'red'}}>
-                {this.props.currentUser}
-              </div>
-              <form onSubmit={this.handleSubmit.bind(this)}>
-                <label htmlFor="email">Email</label>
-                <input placeholder="Email" id="email" type="text" ref="email" className="validate" />
-                <label htmlFor="password">Password</label>
-                <input placeholder="Password" id="password" type="password" className="validate" ref="userPassword" />
-                <button className="btn black" style={{borderRadius: "20px"}} type="submit">Submit</button>
-              </form>
-              <br/>
-              <Link to="/signup" className="center" style={{color: "black"}}>Don't have an account?</Link>
+        <div className="row">
+          <div className="col l6 m6 s6 offset-l3 offset-m3 offset-s3 center">
+            <br />
+            <div style={{color: 'red'}}>
+              {this.props.currentUser}
             </div>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+              <label htmlFor="email">Email</label>
+              <input placeholder="Email" id="email" type="text" ref="email" className="validate" />
+              <label htmlFor="password">Password</label>
+              <input placeholder="Password" id="password" type="password" className="validate" ref="userPassword" />
+              <button className="btn black" style={{borderRadius: "20px"}} type="submit">Submit</button> <br/><br/>
+              <button className="btn black" style={{borderRadius: "20px"}} onClick={this.onClick.bind(this)}>Guest Log In</button>
+            </form>
+            <br/>
+            <Link to="/signup" className="center" style={{color: "black"}}>Don't have an account?</Link>
           </div>
-          </div>
+        </div>
+      </div>
     )
   }
 }
